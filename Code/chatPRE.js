854d72672bba21ead321ae2315e803f379dcf636
Elliecorn98 <elianag30@lakesideschool.org>
2025-02-09 12:26:32 -0800

    Update chatPRE.js brain rot

--- Code/chatPRE.js
+++ Code/chatPRE.js
@@ -916,7 +916,7 @@
         const botMessageRef = push(messagesRef);
         await update(botMessageRef, {
           User: BOT_USERS.RNG,
-          Message: `🎲 Rolling a ${sides}-sided die: ${result}`,
+          Message: `🎲 Rolling a skibbity ${sides}-sided die: ${result}`,
           Date: Date.now(),
         });
       } else {
