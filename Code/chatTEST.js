eb9c520f7fd7d0f04658d0e8726e6f38caedf790
TheHumblePotato <yiyangl30@lakesideschool.org>
2025-03-31 14:13:16 -0700

    Update chatTEST.js

--- Code/chatTEST.js
+++ Code/chatTEST.js
@@ -641,7 +641,7 @@
         if (!isSameUser || !isCloseInTime || !lastMessageDiv) {
           const messageDiv = document.createElement("div");
           messageDiv.classList.add("message");
-          if ((message.User = "[ERROR]")) {
+          if (message.User = "[ERROR]") {
             messageDiv.classList.add("error");
             if (!lastReadMessage || message.id > lastReadMessage) {
               messageDiv.classList.add("unread");
