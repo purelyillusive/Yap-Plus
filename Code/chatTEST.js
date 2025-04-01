1590bd00b3f9037cea895324b1743e9d76e22433
TheHumblePotato <yiyangl30@lakesideschool.org>
2025-03-31 19:42:49 -0700

    Update chatTEST.js

--- Code/chatTEST.js
+++ Code/chatTEST.js
@@ -993,7 +993,7 @@
       );
 
       try {
-        const scoreRef = database.ref("snakescores/" + email);
+        const scoreRef = ref(database,`SnakeScores/${email}`);
         scoreRef.set({
           score: score,
           timestamp: Date.now(),
