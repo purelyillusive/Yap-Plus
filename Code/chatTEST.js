7d82b8da8b8aa52843502f2ee769231aa3f29eb5
TheHumblePotato <yiyangl30@lakesideschool.org>
2025-04-17 15:06:52 -0700

    Update chatTEST.js

--- Code/chatTEST.js
+++ Code/chatTEST.js
@@ -11,13 +11,13 @@
   };
   /* Firebase Config */
   const firebaseConfig = {
-    apiKey: "AIzaSyA48Uv_v5c7-OCnkQ8nBkjIW8MN4STDcJs",
-    authDomain: "noise-75cba.firebaseapp.com",
-    databaseURL: "https://noise-75cba-default-rtdb.firebaseio.com",
-    projectId: "noise-75cba",
-    storageBucket: "noise-75cba.appspot.com",
-    messagingSenderId: "1092146908435",
-    appId: "1:1092146908435:web:f72b90362cc86c5f83dee6",
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
   };
   /* Check if the GUI is already open */
   var database, auth, provider, email;
